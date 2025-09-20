595ad6118e47e62a582d715f423fd0c7a82d0a38
freshstartagain <newstartagainhays@gmail.com>
2025-09-20 12:23:31 +0800

    feat: add profile

--- home/src/App.vue
+++ home/src/App.vue
@@ -3,6 +3,7 @@ import { darkTheme } from "naive-ui"
 import { useThemeStore } from '@/store/theme'
 
 import Experience from '@/components/Experience.vue'
+import Profile from "@/components/Profile.vue"
 import Navbar from '@/components/Navbar.vue'
 
 const theme = useThemeStore()
@@ -10,14 +11,15 @@ const theme = useThemeStore()
 
 <template>
   <n-config-provider :theme="theme.isDark ? darkTheme : null">
-    <n-layout>
-      <n-layout-header>
+    <n-layout style="height: 100vh;">
+      <n-layout-header bordered>
         <Navbar />
       </n-layout-header>
-      <n-layout-content content-style="padding: 24px;">
-        <Experience />
+      <n-layout-content class="min-h-screen overflow-x-hidden">
+        <Profile />
+        <!-- <Experience /> -->
       </n-layout-content>
-      <n-layout-footer>Chengfu Road</n-layout-footer>
+      <n-layout-footer bordered>Chengfu Road</n-layout-footer>
     </n-layout>
   </n-config-provider>
 </template>
---
+++ home/src/components/Profile.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <n-flex justify="center" class="py-24">
+    <n-card class="max-w-2xl">
+      <n-text class="text-lg">
+        Hi, I’m a software engineer with a focus on <strong>backend and infrastructure</strong>. I build
+        end-to-end applications, develop API integrations, and help design automations to store and produce data that
+        support data engineering workflows. I also manage infrastructure on cloud platforms, while guiding a small team
+        and reviewing code to ensure quality and scalability.
+      </n-text>
+    </n-card>
+  </n-flex>
+</template>
