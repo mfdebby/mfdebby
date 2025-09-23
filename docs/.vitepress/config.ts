4f6fe1625144d2726846cb3185b8286a5415c69f
freshstartagain <newstartagainhays@gmail.com>
2025-09-23 13:58:04 +0800

    feat: add logo and favicon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,9 +4,9 @@ export default defineConfig({
   lang: 'en-US',
   title: 'mfdebby',
   description: "mfdebby's site containing notes on anything related to tech.",
-  // head: [["link", { rel: "icon", href: "/logo.png" }]],
+  head: [["link", { rel: "icon", href: "/butus.ico" }]],
   themeConfig: {
-    // logo: "/logo.png",
+    logo: "/butus_logo.png",
     search: {
       provider: 'local',
     },
